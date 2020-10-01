4768308f18d2ec557dae48c19a9ef62daf9b05fb
Shakthi Priya T S <shakthipriyats@gmail.com>
2020-10-01 21:00:52 +0530

    first commit - Basics Course Project

---
+++ basics-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Array<Recipe> = [
+    new Recipe('Test Recipe', 'This is a Test Recipe', 'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/3:2/w_1280,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'),
+    new Recipe('Test Recipe', 'This is a Test Recipe', 'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/3:2/w_1280,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
